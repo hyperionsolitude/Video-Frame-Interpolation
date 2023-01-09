a51de8f618acd014284444d625264a443cd7222e
Utku Acar <work.utkuacar@gmail.com>
2023-01-09 17:01:41 +0300

    reconstruction.py intermediate backup.

---
+++ reconstruction (copy).py
@@ -0,0 +1,27 @@
+import os
+from models.IFRNet import Model
+from utils import read
+import imageio
+from PIL import Image
+import sys
+maxframe= int(sys.argv[1])
+offset= int(sys.argv[2])
+fileList = []
+frame_counter=0
+for file in range(offset+1,maxframe):
+    if (file != 1 and file % 5 != 0 and file % 20 != 0):
+        fileList.append("./res_frames/frame"+str(file)+"_30.png")
+        frame_counter = frame_counter +1
+    if (file % 4 == 0):
+        fileList.append("./res_frames/frame"+str(file)+"_240.png")
+        frame_counter = frame_counter +1
+    if (file ==1 or file % 20 == 0):
+        fileList.append("./res_frames/frame"+str(file)+"_240.png")
+        frame_counter = frame_counter +1
+print(fileList)
+writer = imageio.get_writer('./outputs/output.mp4', fps=30)
+for im in fileList:
+    writer.append_data(imageio.imread(im))
+writer.close()
+print("The video has been saved at ./outputs/output.mp4")
+print("Total Frame of the video is:"+str(frame_counter))
